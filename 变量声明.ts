34c07d4d49f65189ab8d6c1c9755474fac0b6a59
craig0chq0 <705284836@qq.com>
2018-07-29 17:56:51 +0800

    增加了一些题目

---
+++ 变量声明.ts
@@ -0,0 +1,5 @@
+// 声明一个数组 名 arr 元素的值按顺序为 0 1 2 3 4 5 6 7 8
+// 用 const 声明一个对象 名 obj 对象的属性和值分别为 first 0 second 2 third 3 forth 5 fifth 4
+// 交换 obj 对象的 forth和fifth的值
+// 解构声明两个变量 first second ，值为 arr 的 第4，5个元素
+// 写一个函数 解构获取 obj 对象的 third 和 forth 属性 并返回他们的和
