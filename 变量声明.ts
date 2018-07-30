57c94ce6ae1b5f7e42b21c9eeef91be4ea928658
craig0chq0 <705284836@qq.com>
2018-07-30 14:05:18 +0800

    添加备注

--- 变量声明.ts
+++ 变量声明.ts
@@ -1,8 +1,6 @@
 // 声明一个数组 名 arr 元素的值按顺序为 0 1 2 3 4 5 6 7 8
 let arr: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 // 用 const 声明一个对象 名 obj 对象的属性和值分别为 first 0 second 2 third 3 forth 5 fifth 4
-// enum rank{first=0,secong=2,third=3,forth=5,fifth=4}
-// const obj=rank;
 const obj = {
     first: 0, second: 2, third: 3, forth: 5, fifth: 4
 };
@@ -11,8 +9,7 @@ let tem = obj.forth;
 obj.forth = obj.fifth;
 obj.fifth = tem;
 // 解构声明两个变量 first second ，值为 arr 的 第4，5个元素
-// let [, second, , fourth] = ;
-let [, , , first, second] = arr;//此处的arr就代表了{first: 0, second: 2, third: 3, forth: 5, fifth: 4}
+let [, , , , first, second] = arr;//此处的arr就代表了{first: 0, second: 2, third: 3, forth: 5, fifth: 4},
 
 // 写一个函数 解构获取 obj 对象的 third 和 forth 属性 并返回他们的和
 let { third, forth } = obj;
@@ -20,3 +17,5 @@ let { third, forth } = obj;
 function sum({ third, forth } = obj) {
     return third + forth;
 }
+//数组let= [, , , , first, second] = arr 中[, , , , first, second]对应着[, , , ,4,5],位置对应
+//对象{ third, forth } = obj 中 obj即{first: 0, second: 2, third: 3, forth: 5, fifth: 4},third对应third;forth对应forth,民称对应
