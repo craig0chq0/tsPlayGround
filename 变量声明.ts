248b1f0ad271d484b1913088726ef3018012bc4f
craig0chq0 <705284836@qq.com>
2018-07-30 09:57:33 +0800

    完成变量和基础

--- 变量声明.ts
+++ 变量声明.ts
@@ -1,5 +1,18 @@
 // 声明一个数组 名 arr 元素的值按顺序为 0 1 2 3 4 5 6 7 8
+let arr :number[]=[0,1,2,3,4,5,6,7,8];
 // 用 const 声明一个对象 名 obj 对象的属性和值分别为 first 0 second 2 third 3 forth 5 fifth 4
+// enum rank{first=0,secong=2,third=3,forth=5,fifth=4}
+// const obj=rank;
+const obj={
+    first:0,second:2,third:3,forth:5,fifth:4
+}
 // 交换 obj 对象的 forth和fifth的值
+obj.fifth=5;
+obj.forth=4;
 // 解构声明两个变量 first second ，值为 arr 的 第4，5个元素
+let [first,second]=[4,5]
+
 // 写一个函数 解构获取 obj 对象的 third 和 forth 属性 并返回他们的和
+function sum([third,forth]:[3,4]){
+    return third+forth
+}
--- 基础类型.ts
+++ 基础类型.ts
@@ -1,7 +1,17 @@
 // 声明一个数字类型的值
+let a :number;
 // 声明一个字符串类型的值
+let b :string;
 // 声明一个布尔类型的值
+let c :boolean;
 // 用两种方法声明一个字符串类型的数组
+let d :string[];
+let e :Array<string>;
 // 声明一个元组 第一个元素是数字 后两个是字符串
+let f :[number,string,string];
 // 声明一个任意类型的枚举
+enum letter{a,b,c,d}
+let g:letter
 // 声明一个any类型的变量，并赋值为 8，然后声明一个变量， 值为上面的变量的值减 2
+let h:any="abcdefgh"
+let i:number=(<string>h).length-2
